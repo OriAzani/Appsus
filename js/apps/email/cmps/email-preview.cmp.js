a459d8e01aa0c0fd7493086d4e29bd7c8c85848f
Ori <ori.azani@gmail.com>
2020-06-24 21:36:03 +0300

    21:35 24/6

--- js/apps/email/cmps/email-preview.cmp.js
+++ js/apps/email/cmps/email-preview.cmp.js
@@ -1,14 +1,21 @@
 export default {
     props: ['email'],
     template: `
-        <li class="email-preview flex column space-between">
+        <li v-bind:class="{ unread-email: isRead }" :class="unread-email" class="email-preview flex column space-between"  v-on:click="isRead = true">>
             <router-link :to="'/email/' + email.emailId">
            <h3 class="title">{{email.subject}}</h3>
            </router-link>
         </li>
     `,
+    data(){
+        return{
+        isRead: null,
+        }
+    },
  created(){
-     console.log(this.email);
-     
+    // console.log('ggfgf');
+    // console.log(this.email.emailId);
+     this.isRead = email.isRead
+     //console.log(this.isRead)
  }
 };
