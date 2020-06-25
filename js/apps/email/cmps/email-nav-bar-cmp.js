1119e68677fc2f9f3725a69378750f114ebd9b3d
dolev <dolevtsfoni1@gmail.com>
2020-06-25 11:10:45 +0300

    11:10 25/6/2020

--- js/apps/email/cmps/email-nav-bar-cmp.js
+++ js/apps/email/cmps/email-nav-bar-cmp.js
@@ -1,10 +1,36 @@
+import { emailService } from "../services/email-service.js";
 export default {
     template: `
     <nav>
-        <router-link to="email/nav/inbox">inbox</router-link> |
+        <router-link to="email/nav/inbox">inbox{{checkUnred}}</router-link> |
         <router-link to="email/nav/starred">starred</router-link> |
         <router-link to="email/nav/sent">sent Mail</router-link> | 
         <router-link to="email/nav/drafts">drafts</router-link> | 
     </nav>
     `,
+
+    data() {
+        return {
+            emails: [],
+        }
+    },
+    computed: {
+        checkUnred() {
+
+            return this.emails.reduce((unreadCount, email) => {
+                if (!email.isRead) unreadCount++;
+                console.log(unreadCount);
+
+                return unreadCount;
+            }, 0);
+
+        },
+    },
+    created() {
+        emailService.getEmails().then((emails) => {
+            this.emails = emails;
+            console.log(this.emails);
+
+        });
+    }
 }
