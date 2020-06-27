c21797846614933a08e1bdb7197ef8c905d1a6d3
dolev <dolevtsfoni1@gmail.com>
2020-06-28 00:22:33 +0300

    28/6 00:22

--- js/main.js
+++ js/main.js
@@ -6,15 +6,18 @@ new Vue({
     router: myRouter,
     template: `
         <div class="flex column">
+        
             <header class="main-header flex align-center space-between">
                 <h1>AppSusita!</h1>
+                <img src="../img/logo.png" alt="logo" width="150" height="150">
                 <nav-bar/>
             </header>
 
             <main>
                 <router-view />
+                
             </main>
-
+            
             <footer class="flex column">
                <p> Coffeerights 2020 </p>
                <p>Dolev Tsfoni & Ori Azani</p>
