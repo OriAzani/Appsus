e84ec54f5333ab46443db02a482b0c9463360286
dolev <dolevtsfoni1@gmail.com>
2020-06-28 20:40:12 +0300

    20:40

--- js/apps/email/cmps/email-nav-bar-cmp.js
+++ js/apps/email/cmps/email-nav-bar-cmp.js
@@ -1,72 +1,51 @@
+import { emailService } from "../services/email-service.js";
 export default {
-    props: ['emails'],
     template: `
 
     <nav class="email-nav-bar flex column">
         
     <router-link to="email/compose"> <span><i class="fas fa-plus"></i></span>  Compose</router-link> 
 
-        <button class="draftBtn" @click="setStatus('inbox')" > <span><i class="fas fa-inbox"></i></span> Inbox {{checkUnread}} </button>
+        <button class="draftBtn" @click="setStatus('inbox')" > <span><i class="fas fa-inbox"></i></span> Inbox {{inbox}} </button>
         <button class="draftBtn" @click="setStatus('starred')"> <span><i class="far fa-star"></i></span> Starred </button>  
         <button class="draftBtn" @click="setStatus('sent')">  <span> <i class="far fa-share-square"></i> </span> Sent Mail </button>  
         <button class="draftBtn" @click="setStatus('drafts')"> <span><i class="fab fa-firstdraft"></i> </span>Drafts </button>  
    
     </nav>
     `,
+    data() {
+        return {
+            emails: [],
+        };
+
+    },
+    computed: {
+        inbox() {
+
+            var inbox = this.emails;
+            console.log(inbox);
+            inbox = inbox.filter(email => email.isInbox)
+            console.log(inbox);
+
+            // this.inbox.filter(email => email.isInbox)
+            var countUnred = inbox.reduce((acc, email) => {
+                if ((!email.isRead) && (email.isInbox)) acc++;
+                return acc;
+            }, 0);
+            return countUnred;
+        }
+    },
     methods: {
         setStatus(status) {
             this.$emit('setStatus', status)
         }
     },
-    data() {
-        return {
-            emailsStam: this.emails
-
-        };
-
-    },
-    // computed: {
-    //     checkUnread() {
-    //         return this.emails.reduce((unreadCounter, email) => {
-    //             if ((!email.isRead) && (email.isInbox)) unreadCounter++;
-    //             return unreadCounter;
-    //         }, 0);
-
-    //     },
-    // },
     created() {
-        console.log(this.emailsStam);
-        // this.inbox = this.emails.filter(email => email.isInbox)
-        // console.log(this.inbox);
-
+        emailService.getEmails().then((emails) => {
+            this.emails = emails;
+        });
     },
+
 
 
 }
-//    // data() {
-//     //     return {
-//     //         inbox: [],
-
-//     //     };
-
-//     // },
-//     computed: {
-//         checkUnread() {
-//             return this.inbox.reduce((unreadCounter, email) => {
-//                 if (!email.isRead) unreadCounter++;
-//                 return unreadCounter;
-//             }, 0);
-
-//         },
-
-
-//     },
-//     created() {
-//         console.log(this.emails);
-//         // this.inbox = this.emails.filter(email => email.isInbox)
-//         // console.log(this.inbox);
-
-//     },
-
-
-// }
