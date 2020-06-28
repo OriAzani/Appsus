fb1c6970098716a9af90b1b047c4bde6daaabe5d
Ori <ori.azani@gmail.com>
2020-06-28 18:31:05 +0300

    18:32

--- js/apps/email/cmps/email-nav-bar-cmp.js
+++ js/apps/email/cmps/email-nav-bar-cmp.js
@@ -5,7 +5,7 @@ export default {
         
     <router-link to="email/compose"> <span><i class="fas fa-plus"></i></span>  Compose</router-link> 
 
-        <button class="draftBtn" @click="setStatus('inbox')" > <span><i class="fas fa-inbox"></i></span> Inbox: {{checkUnread}} </button>
+        <button class="draftBtn" @click="setStatus('inbox')" > <span><i class="fas fa-inbox"></i></span> Inbox {{checkUnread}} </button>
         <button class="draftBtn" @click="setStatus('starred')"> <span><i class="far fa-star"></i></span> Starred </button>  
         <button class="draftBtn" @click="setStatus('sent')">  <span> <i class="far fa-share-square"></i> </span> Sent Mail </button>  
         <button class="draftBtn" @click="setStatus('drafts')"> <span><i class="fab fa-firstdraft"></i> </span>Drafts </button>  
